0b82085345cff1ae03aace1774aa46831decfa8f
halmi-is <m0000590@naver.com>
2024-11-27 11:59:00 +0900

    메인_프론트_디자인1
    - 메인화면 전체조회 출력
    - 메인테이블 카드 데이터 추가 페이지
    - 카드 번호 클릭 시 상세조회 페이지로 넘어가는건 아직 수정 중 디자인만 완료

---
+++ front/src/views/main/MainDetail.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <!-- name -->
+    <div class="form-floating mb-3">
+      <input type="text" class="form-control" id="name" placeholder="name" v-model="tour.name"/>
+      <label for="name">name</label>
+    </div>
+
+    <!-- location -->
+    <div class="form-floating mb-3">
+      <input type="text" class="form-control" id="location" placeholder="location" v-model="tour.location"/>
+      <label for="location">location</label>
+    </div>
+
+    <!-- description -->
+    <div class="form-floating mb-3">
+      <input type="text" class="form-control" id="description" placeholder="description" v-model="tour.description"/>
+      <label for="description">description</label>
+    </div>
+
+    <!-- price -->
+    <div class="form-floating mb-3">
+      <input type="text" class="form-control" id="price" placeholder="price" v-model="tour.price"/>
+      <label for="price">price</label>
+    </div>
+
+  </div>
+</template>
+<script>
+import MainService from '@/services/main/MainService';
+export default {
+  data() {
+    return {
+        tour:{
+            tourId: "",
+            name:"",
+            location:"",
+            description:"",
+            price:""
+        }
+      }
+    },
+  methods: {
+    async getDetail(tourId) {
+        try {
+          let response = await MainService.get(tourId);
+          console.log(response.data); // 디버깅
+          this.tour = response.data;
+        } catch (error) {
+          console.log(error);
+        }
+      },
+  },
+  mounted() {
+    this.getDetail(this.$route.params.tourId);
+  },
+};
+</script>
+<style></style>
