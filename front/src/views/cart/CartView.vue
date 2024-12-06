cbe630c3a47cc4ffddf0fbe62bf63e8e43ef2f2f
wooseockJang <127363950+wooseockJang@users.noreply.github.com>
2024-12-06 11:06:17 +0900

    카트 수정
    카트 수정

--- front/src/views/cart/CartView.vue
+++ front/src/views/cart/CartView.vue
@@ -1,20 +1,51 @@
 <template>
   <div class="cart-container">
-    <h1 class="cart-title">장바구니(현재:{{ cartCount }})</h1>
-    <button @click="clearAllCart" class="clear-all-button">전체 삭제</button>
+    <h1 class="cart-title">장바구니(현재: {{ cartCount }})</h1>
+
+    <!-- 전체 선택 및 선택 삭제 버튼 -->
+    <div class="cart-actions">
+      <label>
+        <input
+          type="checkbox"
+          @change="toggleSelectAll"
+          :checked="isAllSelected"
+        />
+        전체 선택
+      </label>
+      <button @click="deleteSelectedItems" class="delete-selected-button">
+        선택 삭제
+      </button>
+    </div>
+
+    <!-- 장바구니 아이템 목록 -->
     <div v-if="cart.length > 0" class="cart-items">
       <div class="cart-item" v-for="(data, index) in cart" :key="index">
         <!-- 이미지 섹션 -->
         <div class="cart-item-image-container">
+          <!-- 이미지 상단 왼쪽 체크박스 -->
+          <input
+            type="checkbox"
+            v-model="selectedItems"
+            :value="data.cartId"
+            class="cart-item-checkbox"
+          />
           <img
             :src="data.tourFileUrl"
             alt="Tour Image"
             class="cart-item-image"
+            @click="makeReservation(data)"
           />
         </div>
 
         <!-- 정보 섹션 -->
         <div class="cart-item-details">
+          <button
+            class="cart-item-remove"
+            @click="deleteFromCartId(data.cartId)"
+          >
+            &times;
+            <!-- X 아이콘 -->
+          </button>
           <h2 class="cart-item-title">{{ data.tourName }}</h2>
           <p class="cart-item-room">{{ data.roomName }}</p>
           <p class="cart-item-duration">
@@ -23,25 +54,13 @@
           <p class="cart-item-checkin">
             체크인 : {{ data.checkInDate }} {{ data.checkInTime }}
           </p>
-          <p class="cart-item-checkin">
+          <p class="cart-item-checkout">
             체크아웃 : {{ data.checkOutDate }} {{ data.checkOutTime }}
           </p>
-
           <p class="cart-item-capacity">인원(기준) : {{ data.capacity }}명</p>
           <p class="cart-item-price" style="font-weight: 900; color: #e74c3c">
             총 결제 금액 : {{ data.totalPrice }}
           </p>
-          <div class="cart-item-buttons">
-            <button class="cart-item-buy" @click="makeReservation(data)">
-              결제하기
-            </button>
-            <button
-              class="cart-item-remove"
-              @click="deleteFromCartId(data.cartId)"
-            >
-              삭제
-            </button>
-          </div>
         </div>
       </div>
     </div>
@@ -52,30 +71,17 @@
 </template>
 
 <script>
-import CartService from "@/services/cart/CartService"; // 장바구니 정보 서비스
+import CartService from "@/services/cart/CartService";
 
 export default {
   data() {
     return {
       userEmail: "",
-
+      user: "",
       cart: [],
-
-      cartCount: 0, // 장바구니 개수 세기 변수
-
-      reservation: {
-        totalPrice: "", // 총 금액(1박 금액 * 숙박 일수)
-        userEmail: "", // 유저 이메일
-        stayDuration: 0, // 숙박 일수
-        tourFileUrl: "", // 숙소 이미지
-        tourName: "", // 숙소 명
-        roomName: "", // 방 이름
-        capacity: 0, // 수용 기준 인원
-        checkInDate: "", // 체크인 날짜
-        checkOutDate: "", // 체크아웃 날짜
-        checkInTime: "", // 체크인 시간
-        checkOutTime: "", // 체크아웃 날짜
-      },
+      cartCount: 0, // 장바구니 개수
+      selectedItems: [], // 선택된 cartId 리스트
+      isAllSelected: false, // 전체 선택 상태
     };
   },
   methods: {
@@ -87,77 +93,95 @@ export default {
         console.error("Error fetching cart:", error);
       }
     },
-
     async deleteFromCartId(cartId) {
       try {
-        // 서버에서 삭제 요청
-        const response = await CartService.deleteFromCartId(cartId);
-        console.log(response.data);
+        await CartService.deleteFromCartId(cartId);
         alert("삭제 되었습니다.");
-        // 로컬에서 즉시 삭제 (UI 반영)
-        // filter 메서드는 배열에서 조건에 맞지 않는 항목들을 제거하여 새로운 배열을 생성
-        this.cart = this.cart.filter((item) => item.cartId !== cartId); // 삭제된 항목을 제외한 나머지 항목들만 남김
+        this.cart = this.cart.filter((item) => item.cartId !== cartId);
         this.countCartItems(this.userEmail);
       } catch (error) {
-        console.error("Error fetching cart:", error);
+        console.error("Error deleting item:", error);
       }
     },
+    async deleteSelectedItems() {
+      if (this.selectedItems.length === 0) {
+        alert("선택된 항목이 없습니다.");
+        return;
+      }
 
+      try {
+        await Promise.all(
+          this.selectedItems.map((cartId) =>
+            CartService.deleteFromCartId(cartId)
+          )
+        );
+        alert("선택된 항목이 삭제되었습니다.");
+        this.cart = this.cart.filter(
+          (item) => !this.selectedItems.includes(item.cartId)
+        );
+        this.selectedItems = []; // 선택 항목 초기화
+        this.isAllSelected = false; // 전체 선택 초기화
+        this.countCartItems(this.userEmail);
+      } catch (error) {
+        console.error("Error deleting selected items:", error);
+      }
+    },
+    toggleSelectAll() {
+      if (this.isAllSelected) {
+        this.selectedItems = []; // 전체 선택 해제
+      } else {
+        this.selectedItems = this.cart.map((item) => item.cartId); // 전체 선택
+      }
+      this.isAllSelected = !this.isAllSelected;
+    },
     async clearAllCart() {
       try {
-        // 서버에서 삭제 요청
-        const response = await CartService.deleteFromCartEmail(this.userEmail);
-        console.log(response.data);
-
-        alert("전체삭제 되었습니다.");
-        // 서버에서 삭제가 성공적으로 처리되면 클라이언트에서 장바구니 비우기
-        this.cart = []; // 장바구니 배열을 빈 배열로 설정하여 즉시 UI에 반영
+        await CartService.deleteFromCartEmail(this.userEmail);
+        alert("전체 삭제 되었습니다.");
+        this.cart = [];
         this.countCartItems(this.userEmail);
       } catch (error) {
         console.error("Error clearing cart:", error);
       }
     },
-
     async countCartItems(userEmail) {
       try {
-        // 서버에서 삭제 요청
         const response = await CartService.countCartItems(userEmail);
         this.cartCount = response.data;
         localStorage.setItem("cartCount", this.cartCount.toString());
-        // **객체나 배열 같은 복잡한 데이터는 JSON.stringify()를 사용하고, 숫자나 문자열 같은 단순한 데이터는 toString()을 사용해도 됨**
       } catch (error) {
-        console.error("Error clearing cart:", error);
+        console.error("Error counting cart items:", error);
       }
     },
-
     makeReservation(data) {
-      this.reservation.totalPrice = data.totalPrice;
-      this.reservation.userEmail = data.userEmail;
-      this.reservation.stayDuration = data.stayDuration;
-      this.reservation.tourFileUrl = data.tourFileUrl;
-      this.reservation.tourName = data.tourName;
-      this.reservation.roomName = data.roomName;
-      this.reservation.capacity = data.capacity;
-      this.reservation.checkInTime = data.checkInTime;
-      this.reservation.checkOutTime = data.checkOutTime;
-      this.reservation.checkInDate = data.checkInDate;
-      this.reservation.checkOutDate = data.checkOutDate;
+      const reservation = {
+        totalPrice: data.totalPrice,
+        userEmail: data.userEmail,
+        stayDuration: data.stayDuration,
+        tourFileUrl: data.tourFileUrl,
+        tourName: data.tourName,
+        roomName: data.roomName,
+        capacity: data.capacity,
+        checkInDate: data.checkInDate,
+        checkInTime: data.checkInTime,
+        checkOutDate: data.checkOutDate,
+        checkOutTime: data.checkOutTime,
+      };
 
-      // 각 방에 대해 독립적인 예약할 정보 저장
       localStorage.setItem(
         `reservation_${data.cartId}`,
-        JSON.stringify(this.reservation)
+        JSON.stringify(reservation)
       );
 
-      // 예약 페이지로 이동
       this.$router.push(`/cart/payment/${data.cartId}`);
     },
   },
   mounted() {
-    const user = localStorage.getItem("user"); // 저장된 사용자 정보 가져오기
+    const user = localStorage.getItem("user");
     if (user) {
-      const parsedUser = JSON.parse(user); // JSON 문자열을 객체로 파싱
-      this.userEmail = parsedUser.email; // 이메일 정보 저장
+      const parsedUser = JSON.parse(user);
+      this.user = parsedUser;
+      this.userEmail = parsedUser.email;
     } else {
       console.error("No user data found in localStorage.");
     }
@@ -185,23 +209,26 @@ export default {
   color: #333;
 }
 
-.clear-all-button {
-  padding: 12px 24px; /* 적당히 큰 버튼 */
-  background-color: #e74c3c;
-  color: white;
-  border: none;
-  border-radius: 25px; /* 둥글게 */
-  font-size: 1.2rem;
-  cursor: pointer;
-  width: auto;
-  margin: 0 auto 20px;
-  display: block;
-  transition: background-color 0.3s, transform 0.2s ease;
+/* 전체 선택/삭제 버튼 */
+.cart-actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
 }
 
-.clear-all-button:hover {
-  background-color: #c0392b;
-  transform: scale(1.05); /* 호버 시 크기 약간 증가 */
+.delete-selected-button {
+  color: #3498db; /* 파란색 글씨 */
+  background-color: transparent; /* 배경색 제거 */
+  border: none;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: color 0.3s, transform 0.2s ease;
+}
+
+.delete-selected-button:hover {
+  color: #2980b9; /* 호버 시 글씨 색 변경 */
+  transform: scale(1.05); /* 호버 시 약간 확대 */
 }
 
 .cart-items {
@@ -212,34 +239,53 @@ export default {
 
 .cart-item {
   display: flex;
+  position: relative; /* 상대 위치 설정 */
   background-color: white;
   padding: 15px;
-  border-radius: 12px; /* 둥근 모서리 */
+  border-radius: 12px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   transition: transform 0.2s ease, box-shadow 0.2s ease;
 }
 
 .cart-item-image-container {
   width: 70%;
-  height: auto;
   overflow: hidden;
   border-radius: 8px;
+  position: relative; /* 체크박스 위치 지정 */
 }
 
 .cart-item-image {
   width: 100%;
-  height: 100%;
+  height: auto;
   object-fit: cover;
 }
 
+/* 이미지 상단 왼쪽 체크박스 */
+.cart-item-image-container input[type="checkbox"] {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  width: 20px;
+  height: 20px;
+  z-index: 2;
+}
+
 .cart-item-details {
-  width: 45%;
-  padding-left: 15px;
-  padding-right: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: auto;
+  position: relative; /* 상대적 위치를 설정 */
+  padding: 20px;
+  background-color: #f9f9f9;
+  border-radius: 8px;
+}
+
+.cart-item-remove {
+  position: absolute; /* 절대 위치 */
+  top: 5px; /* 상단에서 5px */
+  right: 15px; /* 우측에서 5px */
+  background: none; /* 배경 제거 */
+  border: none; /* 테두리 제거 */
+  font-size: 1.8rem; /* 글씨 크기 감소 */
+  cursor: pointer; /* 커서 변경 */
+  transition: color 0.2s ease;
 }
 
 .cart-item-title {
@@ -266,34 +312,13 @@ export default {
   justify-content: flex-start;
 }
 
-.cart-item-buy,
-.cart-item-remove {
-  padding: 12px 24px;
-  font-size: 1.1rem;
-  border-radius: 25px; /* 둥글고 귀여운 버튼 */
+.cart-item-buy {
+  padding: 10px 20px;
+  font-size: 1rem;
+  border-radius: 25px;
   border: none;
   cursor: pointer;
   transition: background-color 0.3s, transform 0.2s ease;
-}
-
-.cart-item-buy {
-  background-color: #2ecc71;
-  color: white;
-}
-
-.cart-item-buy:hover {
-  background-color: #2980b9;
-  transform: scale(1.05);
-}
-
-.cart-item-remove {
-  background-color: #e74c3c;
-  color: white;
-}
-
-.cart-item-remove:hover {
-  background-color: #c0392b;
-  transform: scale(1.05);
 }
 
 .cart-empty {
